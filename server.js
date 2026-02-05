72a48ddf5423dee0831dc97ac33825cd93060610
neetuPA <you@example.com>
2026-02-05 18:36:43 +0530

    chnages

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const dotenv=require('dotenv');
 const Dbconnection =require('./connectiondb');
 const taskRoutes = require("./routes/taskRoutes")
-
+const cors =require('cors');
 const app = express();
 dotenv.config();
 Dbconnection();
-
+app.use(cors({
+  origin: "*",  
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 app.use(express.json());
 app.use("/api", taskRoutes);
 app.listen(3000, () => {
