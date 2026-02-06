c88e1f682d4b621fceabe0c9f8032fce906c9b78
neetuPA <you@example.com>
2026-02-06 18:25:15 +0530

    resolve merge conflict

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -47,7 +47,11 @@ exports.updateTaskStatus = async (req, res) => {
     try {
         const { id } = req.params;
         const { status } = req.body;
-        const task = await Task.findOne({ id });
+      const task = await Task.findByIdAndUpdate(
+      id,
+      { status },
+      { new: true }
+    );
         if (!task) {
             return res.status(404).json({ message: "Task not found" });
         }
